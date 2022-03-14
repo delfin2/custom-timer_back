6de4c8b2c32d79b3d2276fbacc7816378399e892
Aleksandr Kolontai <kav993@gmail.com>
2022-03-14 12:08:20 +0200

    bind routes to model

--- routes.js
+++ routes.js
@@ -12,9 +12,26 @@ router.get('/', (req, res) => {
   res.send('Hello browser')
 })
 
-router.post('/', (req, res) => {
-  model.Timer.build(req.body).save()
-  res.send(req.body)
+router.get('/timers', async (req, res) => {
+  const timers = await model.Timer.findAll()
+  res.send(timers)
+})
+
+router.post('/timers', async (req, res) => {
+  const timer = await model.Timer.create(req.body)
+  res.send(timer)
+})
+
+router.put('/timers/:id', async (req, res) => {
+  const target = await model.Timer.findByPk(req.params.id)
+  await target.update(req.body)
+  res.send(target)
+})
+
+router.delete('/timers/:id', async (req, res) => {
+  const target = await model.Timer.findByPk(req.params.id)
+  await target.destroy()
+  res.send(target)
 })
 
 module.exports = router
